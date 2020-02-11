2580380c0a64b2c79c0466cb3918ed5a69157b92
Hcama <hcama1985@outlook.com>
2020-02-11 15:03:59 -0500

    1.5

---
+++ comData.MvcClient/Controllers/NavController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comData.MvcClient.Controllers
+{
+    public class NavController : Controller
+    {
+        //
+        // GET: /Nav/
+
+        public PartialViewResult Menu()
+        {
+            return PartialView();
+        }
+
+    }
+}
